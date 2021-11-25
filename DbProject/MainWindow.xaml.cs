72deb105028d5155617246a8987f0e08b8fd0098
KarelovaOA <93170543+KarelovaOA@users.noreply.github.com>
2021-11-25 07:55:01 +0300

    Add files via upload

---
+++ DbProject/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data;
+
+namespace DbProject
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCheck_Click(object sender, RoutedEventArgs e)
+        {
+
+            Global global = new Global();
+
+            if (!string.IsNullOrEmpty(global.CheckConnection()))
+            {
+                MessageBox.Show("Подключение прошло успешно!");
+            }
+            else
+            {
+                MessageBox.Show("Подключение не удалось!");
+            }
+
+        }
+    }
+}
